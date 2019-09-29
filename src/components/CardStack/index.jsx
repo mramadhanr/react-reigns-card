2045b28e1992398b752f58ea1fd7a227f1720ba1
Ramadhan4 <muhamadramadhanrahmat@gmail.com>
2019-09-29 11:26:34 +0700

    Add CardStack component, is where stack and card collection is managed

---
+++ src/components/CardStack/index.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Card from '../Card';
+
+const CardStack = () => {
+  return (
+    <div id="stack">
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+    </div>
+  );
+};
+
+export default CardStack;
